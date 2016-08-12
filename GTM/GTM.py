Log-likelihood convergence fixed.

GTM/GTM.py
@@ -32,6 +32,7 @@ class GTM(object):
         self.gtm_distance = np.zeros((self.latent_space_size, self.input_data.shape[0]))
         self.gtm_responsibility = np.zeros((self.latent_space_size, self.input_data.shape[0]))
         self.centered_input_data = scale(self.input_data)
+        self.input_reconstructed = np.zeros((self.latent_space_size, self.input_data.shape[1], iterations))
 
     @staticmethod
     def gtm_rectangular(dimension):
@@ -129,8 +130,8 @@ class GTM(object):
         self.gtm_responsibility = np.exp((-beta / 2) * self.gtm_distance)
         responsibility_sum = np.sum(self.gtm_responsibility, 0)
         self.gtm_responsibility = self.gtm_responsibility / np.transpose(responsibility_sum[:, None])
-        log_likelihood = np.sum(np.log(responsibility_sum)) + self.gtm_distance.shape[1] * \
-            ((self.input_data.shape[1] / 2) * np.log(beta / (2 * np.pi)) - np.log(self.gtm_distance.shape[0]))
+        log_likelihood = np.sum(np.log(responsibility_sum) + dist_corr * (-beta/2)) + self.gtm_distance.shape[1] * \
+            ((self.input_data.shape[1] / 2.) * np.log(beta / (2 * np.pi)) - np.log(self.gtm_distance.shape[0]))
         return log_likelihood
 
     def gtm_training(self):
@@ -151,7 +152,7 @@ class GTM(object):
             # Printing diagnostic info
             print "Cycle: %d\t log likelihood: %f\t Beta: %f\n " % (i, float(log_likelihood), beta)
             # Calculate matrix to be inverted
-            lbda = self.regularization * np.ones((self.fi.shape[1], self.fi.shape[1]))
+            lbda = self.regularization * np.eye(self.fi.shape[1], self.fi.shape[1])
             intermediate_matrix = np.dot(np.transpose(self.fi), np.diag(np.sum(self.gtm_responsibility, 1)))
             maximization_matrix = np.dot(intermediate_matrix, self.fi) + lbda / beta
             inv_maximization_matrix = np.linalg.pinv(maximization_matrix)
@@ -159,8 +160,8 @@ class GTM(object):
                                                                                      self.centered_input_data)))
             self.gtm_distance = cdist(np.dot(self.fi, w), self.centered_input_data, 'sqeuclidean')
             input_data_size = self.input_data.shape[0] * self.input_data.shape[1]
-            termc = np.sum(self.gtm_distance * self.gtm_responsibility)
             beta = input_data_size / np.sum(self.gtm_distance * self.gtm_responsibility)
+            self.input_reconstructed[:, :, i] = np.dot(self.fi, w)
         return w, beta, log_likelihood_evol
 
     def gtm_mean(self, w, beta):

GTM/GTM_Running.py
@@ -6,13 +6,14 @@ import networkx as nx
 import numpy as np
 from SSIM import compute_ssim
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import animation
 
 # Importing a multi-mode data set from MATLAB
 input_structure = sio.loadmat('swiss_roll_data.mat')
 input_data = input_structure['X']
-input_data = input_data[0:10000, :]
+input_data = input_data[0:1000, :]
 idx = input_structure['t']
-idx = idx[:, 0:10000]
+idx = idx[:, 0:1000]
 idx = np.argsort(idx)
 input_data = input_data[idx[0, :], :]
 t = np.arange(input_data.shape[0])
@@ -26,7 +27,7 @@ t = np.arange(input_data.shape[0])
 # plt.show()
 
 # GTM training
-test = GTM(input_data, latent_space_size=3600, rbf_number=64, regularization=0.0001, rbf_width=1, iterations=1000)
+test = GTM(input_data, latent_space_size=3600, rbf_number=64, regularization=0.001, rbf_width=2, iterations=100)
 [w2, beta2, log_likelihood_evol2] = test.gtm_training()
 
 # Mean and mode plots for all samples
@@ -34,18 +35,42 @@ fig1 = plt.figure()
 means2 = test.gtm_mean(w2, beta2)
 modes2 = test.gtm_mode(w2)
 plt.scatter(means2[:, 0], means2[:, 1], c=t)
-plt.savefig("py_mean_225.png")
+# plt.savefig("py_mean_225.png")
 
 fig2 = plt.figure()
 plt.scatter(modes2[:, 0], modes2[:, 1],  c=t)
-plt.savefig("py_mode_225.png")
+# plt.savefig("py_mode_225.png")
 
 # Probability distribution plots for all samples
 fig3 = plt.figure()
 test.gtm_pdf()
-plt.savefig("py_pdf_225.png")
-plt.show()
+# plt.savefig("py_pdf_225.png")
 
+# # Manifold plot
+# X = test.input_reconstructed[:, 0,  0].reshape((60, 60))
+# Y = test.input_reconstructed[:, 1,  0].reshape((60, 60))
+# Z = test.input_reconstructed[:, 2,  0].reshape((60, 60))
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1)
+# ax.scatter(test.centered_input_data[:, 0], test.centered_input_data[:, 1], test.centered_input_data[:, 2], c='r')
+# plt.show()
+
+# Animated Manifold Learning
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+wframes = []
+for i in xrange(0, test.iterations):
+    x = test.input_reconstructed[:, 0,  i].reshape((60, 60))
+    y = test.input_reconstructed[:, 1,  i].reshape((60, 60))
+    z = test.input_reconstructed[:, 2,  i].reshape((60, 60))
+    wframe = ax.plot_wireframe(x, y, z, rstride=1, cstride=1)
+    wframes.append([wframe])
+
+anim = animation.ArtistAnimation(fig, wframes, interval=20, blit=True)
+anim.save('manifold.gif', writer='imagemagick', fps=30, extra_args=['-vcodec', 'libx264'])
+plt.show()
 # # Similarity matrix calculation and plot for all samples
 # fig3 = plt.figure()
 # simple_matrix = test.similarity_matrix()